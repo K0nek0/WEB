02c9012ff6e64cd11d83c5ce556264640a52eb65
Timoshchenko Nikolay <silent_master@mail.ru>
2024-03-08 14:51:20 +0200

    task_4 (degree)

--- lab_1/code/index.php
+++ lab_1/code/index.php
@@ -9,7 +9,6 @@ $order .= " and a large soda";
 echo "\nYour order is: $very_bad_unclear_name.\n";
 
 //------
-
 echo "\nTask №2 (numbers)\n";
 
 $num_1 = 4014;
@@ -31,7 +30,6 @@ $this_month = 1089.98;
 echo "\nРазница между расходами в прошлом и текущем месяце: ", $last_month - $this_month . "\n";
 
 //------
-
 echo "\nTask №3 (multiplication and division)";
 
 $num_languages = 4;
@@ -39,5 +37,10 @@ $months = 11;
 $days = 16 * $months;
 $days_per_language = $days / $num_languages;
 echo "\nДней в среднем на изучение каждого языка: ", $days_per_language . "\n";
+
+//------
+echo "\nTask №4 (degree)\n";
+
+echo "8^2 = ", 8 ** 2;
 
 //------
