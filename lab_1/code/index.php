017760d76d63439807acfc25d28530d818b09bf2
Timoshchenko Nikolay <silent_master@mail.ru>
2024-03-08 14:22:55 +0200

    task_1 (access by link)

--- lab_1/code/index.php
+++ lab_1/code/index.php
@@ -1,3 +1,9 @@
 <?php
-echo phpinfo();
+echo "Task №1 (Access by link)";
 
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = &$very_bad_unclear_name;
+$order .= " and a large soda";
+
+echo "\nYour order is: $very_bad_unclear_name.";
