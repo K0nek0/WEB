b71b7ee675a432faf864084de337f7229a04ed67
Timoshchenko Nikolay <silent_master@mail.ru>
2024-03-30 16:53:26 +0200

    task_3 (table) and screenshot

--- lab_3/task_3 (files)/code/index.php
+++ lab_3/task_3 (files)/code/index.php
@@ -8,35 +8,68 @@
     <title>Avito</title>
 </head>
 <body>
-    <form method="post" action="save.php">
-        <label for="email">Email</label>
-        <label>
-            <input type="email" name="email" required>
-        </label>
+    <div id="form">
+        <form method="post" action="save.php">
+            <label for="email">Email</label>
+            <label>
+                <input type="email" name="email" required>
+            </label>
 
-        <label for="categories">Category</label>
-        <label>
-            <select name="categories" required>
-                <?php
-                $files = scandir("./categories");
-                foreach ($files as $file)
-                    if ('.' != $file && '..' != $file)
-                        echo "<option value='$file'>$file</option>";
-                ?>
-            </select>
-        </label>
+            <label for="categories">Category</label>
+            <label>
+                <select name="categories" required>
+                    <?php
+                    $files = scandir("./categories");
+                    foreach ($files as $file)
+                        if ('.' != $file && '..' != $file)
+                            echo "<option value='$file'>$file</option>";
+                    ?>
+                </select>
+            </label>
 
-        <label for="title">Title</label>
-        <label>
-            <input type="text" name="title" required>
-        </label>
+            <label for="title">Title</label>
+            <label>
+                <input type="text" name="title" required>
+            </label>
 
-        <label for="text">Text</label>
-        <label>
-            <textarea rows="5" name="text"></textarea>
-        </label>
+            <label for="text">Description</label>
+            <label>
+                <textarea rows="5" name="text"></textarea>
+            </label>
 
-        <input type="submit" value="Save">
+            <input type="submit" value="Save">
 
-    </form>
+        </form>
+    </div>
+    <div id="table">
+        <table>
+            <thead>
+            <th>Category</th>
+            <th>Title</th>
+            <th>Description</th>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($files as $category){
+                foreach ($files as $category){
+                    if ($category == '.' || $category == '..') continue;
+                    $titles = scandir("./categories/$category");
+                    foreach ($titles as $title){
+                        if ($title == '.' || $title == '..') continue;
+                        $newTitle = substr($title, 0, strlen($title) - 4);
+                        $desc = file_get_contents("./categories/$category/$title");
+                        echo "<tr>";
+                        echo "<td>$category</td>";
+                        echo "<td>$newTitle</td>";
+                        echo "<td>$desc</td>";
+                        echo "</tr>";
+                    }
+                }
+                break;
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
 </body>
+</html>
