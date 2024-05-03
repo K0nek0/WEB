f67b51dec1b892be3dfe2eb3edad40751598738d
Timoshchenko Nikolay <silent_master@mail.ru>
2024-05-03 22:31:44 +0200

    algorithm for receive and add

--- lab_4/code/index.php
+++ lab_4/code/index.php
@@ -49,18 +49,31 @@
                 require 'vendor/autoload.php';
                 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
 
-                $client = new \Google_Client();
+                $client = new Google_Client();
                 $client->setApplicationName('php-lab-4');
                 $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
                 $client->setAccessType('offline');
                 $path = 'credentials.json';
                 $client->setAuthConfig($path);
 
-                $service = new \Google_Service_Sheets($client);
+                $service = new Google_Service_Sheets($client);
+
+                $spreadsheetId = '1ruEPHBsrQh9OfpIxJhbf9jwouhIrIRcPZ2q0GavMbFQ';
+                $spreadsheet = $service->spreadsheets->get($spreadsheetId);
+                $sheets = $spreadsheet->getSheets();
+                foreach ($sheets as $sheet) {
+                    $sheetTitle = $sheet->getProperties()->getTitle();
+
+                    $response = $service->spreadsheets_values->get($spreadsheetId, $sheetTitle);
+                    $values = $response->getValues();
+                    if (!empty($values) && count($values) >= 4) {
+                        $category = htmlspecialchars($values[0][0]);
+                        $title = htmlspecialchars($values[1][0]);
+                        $email = htmlspecialchars($values[2][0]);
+                        $description = htmlspecialchars($values[3][0]);
+
                     }
                 }
-                break;
-            }
             ?>
             </tbody>
         </table>
--- lab_4/code/save.php
+++ lab_4/code/save.php
@@ -3,10 +3,36 @@ require 'vendor/autoload.php';
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
 
 
-$client = new \Google_Client();
+$client = new Google_Client();
 $client->setApplicationName('php-lab-4');
 $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
 $client->setAccessType('offline');
 $client->setAuthConfig('credentials.json');
 
-$service = new \Google_Service_Sheets($client);
+$service = new Google_Service_Sheets($client);
+
+$spreadsheetId = '1ruEPHBsrQh9OfpIxJhbf9jwouhIrIRcPZ2q0GavMbFQ';
+
+if ('POST' === $_SERVER['REQUEST_METHOD']) {
+    $category = $_POST['categories'] ?? '';
+    $title = $_POST['title'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $description = $_POST['text'] ?? '';
+    $data = [
+        [$category, $title, $email, $description]
+    ];
+}
+
+$range = 'sheet_1';
+
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $data
+]);
+$params = [
+    'valueInputOption' => 'RAW'
+];
+
+$result = $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
+header('Location: index.php');
+exit();
